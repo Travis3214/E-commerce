217a94a557056a5332db3911f26acaafb35c4207
Travis <travis3214@yahoo.com>
2023-08-08 19:04:34 -0400

    added a post to create new categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -7,7 +7,7 @@ router.get('/', async (req, res) => {
   // find all categories
   // be sure to include its associated Products
   try {
-    const categoryData = await Category.findAll({
+    const allData = await Category.findAll({
       include: [
         {
           model: Product,
@@ -15,7 +15,7 @@ router.get('/', async (req, res) => {
         }
       ]
     })
-    res.status(200).json(categoryData)
+    res.status(200).json(allData)
   } catch(err) {
     res.status(400).json(err)
   }
@@ -25,7 +25,7 @@ router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
   try {
-    const categoryData = await Category.findByPk(req.params.id, {
+    const idData = await Category.findByPk(req.params.id, {
       include: [
         {
           model: Product,
@@ -33,18 +33,26 @@ router.get('/:id', async (req, res) => {
         }
       ]
     })
-    if(!categoryData){
+    if(!idData){
       res.status(404).json({ message: 'There is no category with this id!' });
       return;
     }
-    res.status(200).json(categoryData)
+    res.status(200).json(idData)
   } catch(err) {
     res.status(400).json(err)
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  try{
+    const newData = await Category.create({
+      category_name: req.body.category_name
+    })
+    res.status(200).json(newData)
+  } catch (err){
+    res.status(400).json(err)
+  }
 });
 
 router.put('/:id', (req, res) => {
